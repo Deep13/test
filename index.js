d88f06d26e0e21bbbb819256e0af0ae3e1e3d7e4
Deep13 <deep.deepak30@gmail.com>
2025-01-10 20:26:11 +0530

    changes for port

--- index.js
+++ index.js
@@ -40,7 +40,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
